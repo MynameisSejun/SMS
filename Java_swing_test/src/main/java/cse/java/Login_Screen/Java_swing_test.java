3747c8eca29c2934be31de91521666eda4049b49
nyeonggyeong <146078760+nyeonggyeong@users.noreply.github.com>
2023-11-13 00:18:43 +0900

    Add files via upload

---
+++ Java_swing_test/src/main/java/cse/java/Login_Screen/Java_swing_test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cse.java.Login_Screen;
+
+/**
+ *
+ * @author suk22
+ */
+public class Java_swing_test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
