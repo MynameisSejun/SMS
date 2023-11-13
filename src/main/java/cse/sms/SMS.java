5d97d6e54ff0cea5e376455ad2bc29f9d3155e6f
MynameisSejun <parkpsj0418@naver.com>
2023-11-14 00:21:57 +0900

    main 함수 추가

---
+++ src/main/java/cse/sms/SMS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cse.sms;
+
+import cse.sms.view.Login_Page;
+
+/**
+ *
+ * @author 915
+ */
+public class SMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login_Page a = new Login_Page();
+        a.run();
+    }
+    
+}
